c513c2dc77a18a603c9864920b4239e56dad87ff
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-08-08 12:17:13 +0200

    improved frontend, added GUI window like aspect

--- DB_Classifier_System/settings.py
+++ DB_Classifier_System/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "db_classifier_app",
 ]
 
 MIDDLEWARE = [
@@ -115,8 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'db_classifier_app/static',
+]
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- db_classifier_app/views.py
+++ db_classifier_app/views.py
@@ -7,16 +7,26 @@ from django.shortcuts import render
 
 
 def index(request):
+    rows = ""
+    data_structure = ""
+    server = ""
+    top_databases = None
+
     if request.method == "POST":
         rows = int(request.POST["rows"])
         data_structure = int(request.POST["data_structure"])
         server = request.POST["server"]
 
         top_databases = recommend_database_v2(rows, data_structure, server)
-    else:
-        top_databases = None
+        print(top_databases)
 
-    return render(request, 'db_classifier_app/index.html', {'top_databases': top_databases})
+    return render(request, 'db_classifier_app/index.html', {
+        'top_databases': top_databases,
+        'rows': rows,
+        'data_structure': data_structure,
+        'server': server
+    })
+
 
 def recommend(request):
     if request.method == "POST":
