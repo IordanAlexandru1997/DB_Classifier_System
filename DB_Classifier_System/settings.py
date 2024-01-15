10f10e14a7d41fedfdcf36f85a4ece97154c9de8
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2024-01-15 12:16:04 +0100

    installed gunicorn

--- DB_Classifier_System/settings.py
+++ DB_Classifier_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c-(rfb*pfwle83ijm*p9x2oj(n2@-c2c3haj@lssnj9k^f$fx9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dbclassifiersystem.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
