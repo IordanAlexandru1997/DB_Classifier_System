802449968f63fc1600b3890f39b62995b60eeca6
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2024-01-15 13:08:17 +0100

    configured static files

--- DB_Classifier_System/settings.py
+++ DB_Classifier_System/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-c-(rfb*pfwle83ijm*p9x2oj(n2@-c2c3haj@lssnj9k^f$fx9"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'db_classifier_app/static',
